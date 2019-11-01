ce0c5e8128c5cfd9c6a69be6e6fc4b592813d699
Arkam Mazrui <superarkam@gmail.com>
2019-10-31 20:25:06 -0400

    Added piece placement functionality

--- Main.js
+++ Main.js
@@ -1,6 +1,8 @@
 class Drawer { //This class takes care of drawing objects to the screen
 	constructor (context) {
 		this.c = context;
+		this.blackPiece = new FillDot(0, 0, "black", 8);
+		this.whitePiece = new FillDot(0, 0, "#D3D3D3", 8);
 	}
 	
 	drawSquare(square) {
@@ -12,8 +14,19 @@ class Drawer { //This class takes care of drawing objects to the screen
 			this.drawSquare(board.boxes[i]);
 		}
 		
+		var dot = 0;
+		
 		for (var i = 0; i < board.dots.length; i++) {
-			this.drawDot(board.dots[i]);
+			dot = board.dots[i];
+			this.drawDot(dot);
+			if (dot.state == 1) {
+				this.blackPiece.setPos(dot.x, dot.y);
+				this.drawFillDot(this.blackPiece);
+			}
+			else if (dot.state == 2) {
+				this.whitePiece.setPos(dot.x, dot.y);
+				this.drawFillDot(this.whitePiece);
+			}
 		}
 		
 		for (var i = 0; i < board.lines.length; i++) {
@@ -32,6 +45,13 @@ class Drawer { //This class takes care of drawing objects to the screen
 		this.c.beginPath();
 		this.c.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
 		this.c.stroke();
+	}
+	
+	drawFillDot(dot) {
+		this.c.beginPath();
+		this.c.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
+		this.c.fillStyle = dot.color;
+		this.c.fill();
 	}
 }
 
@@ -72,6 +92,27 @@ class Dot {
 		this.size = 2;
 		this.state = 0;
 	}
+	
+	setSize(size) {
+		this.size = size;
+	}
+	
+	setPos(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	setState(state) {
+		this.state = state;
+	}
+}
+
+class FillDot extends Dot {
+	constructor(x,y, color, size) {
+		super(x, y);
+		this.setSize(size);
+		this.color = color;
+	}
 }
 
 class Line {
@@ -104,8 +145,6 @@ class Mouse {//Class that keeps track of mouse movement
 class Board { //Class that holds the board's appearance and provides functionality allowing you to get which dot on the board was clicked
 	constructor(canvas) {
 		this.canvas = canvas;
-		this.mouseX = 0;
-		this.mouseY = 0;
 		const w = canvas.offsetWidth;
 		const h = canvas.offsetHeight;
 		const midX = Math.round(w / 2);
@@ -132,6 +171,7 @@ class Board { //Class that holds the board's appearance and provides functionali
 		this.lines = [new Line(box[2].midX, box[2].y, box[0].midX, box[0].y), new Line(box[2].x, box[2].midY, box[0].x, box[0].midY),
 						new Line(box[2].right, box[2].midY, box[0].right, box[0].midY), new Line(box[2].midX, box[2].bottom, box[0].midX, box[0].bottom)];
 					
+		
 		this.rows = [];
 		
 		for (var i = 0; i < dots.length; i+=3) {
@@ -147,13 +187,27 @@ class Board { //Class that holds the board's appearance and provides functionali
 		console.log(this.rows);
 	}
 	
+}
+
+class Game {
+	constructor(board, canvas, drawer) {
+		this.board = board;
+		this.canvas = canvas;
+		this.drawer = drawer;
+		this.mouseX = 0;
+		this.mouseY = 0;
+		this.changeDetected = false;
+		this.lastPiecePlayed = 0;
+		
+	}
+	
 	dotCoodAtMouse(){//Returns which dot was clicked as an (x,y) coordinate from this.rows[][] 2D array
 		var hitbox = 10;
 		var dot = 0;
 		
-		for (var x = 0; x < this.rows.length; x++) {
-			for (var y = 0; y < this.rows[x].length; y++) {
-				dot = this.rows[x][y];
+		for (var x = 0; x < this.board.rows.length; x++) {
+			for (var y = 0; y < this.board.rows[x].length; y++) {
+				dot = this.board.rows[x][y];
 				if (this.mouseX >= dot.x - hitbox && this.mouseX <= dot.x + hitbox && this.mouseY >= dot.y - hitbox && this.mouseY <= dot.y + hitbox) {
 					console.log("Clicked " + dot.x + " " + dot.y + " at " + x + ", " + y);
 					return [x, y];
@@ -167,8 +221,8 @@ class Board { //Class that holds the board's appearance and provides functionali
 		var hitbox = 10;
 		var dot = 0;
 		
-		for (var i = 0; i < this.dots.length; i++) {
-			dot = this.dots[i];
+		for (var i = 0; i < this.board.dots.length; i++) {
+			dot = this.board.dots[i];
 			if (this.mouseX >= dot.x - hitbox && this.mouseX <= dot.x + hitbox && this.mouseY >= dot.y - hitbox && this.mouseY <= dot.y + hitbox) {
 				console.log("Clicked " + dot.x + " " + dot.y + " at " + i);
 				return i;
@@ -180,13 +234,41 @@ class Board { //Class that holds the board's appearance and provides functionali
 	onClick(evt) { //TODO: This function is triggered whenever the user clicks on the board
 	//TODO: This function will be the main functionality for this program; most things will happen in here
 	//Most likely the game will be conditions checking the game state and reacting accordingly to if the user clicked a dot to play in
+		const BLACK_PLACED = 1;
+		const WHITE_PLACED = 2;
+		const NONE_PLACED = 0;
+		var state = 0;
+		
 		var rect = this.canvas.getBoundingClientRect();
 		this.mouseX = evt.clientX - rect.left;
 		this.mouseY = evt.clientY - rect.top;
 		var dotCoods = this.dotCoodAtMouse(); //Use this to get the coordinate of the dot clicked; returns null if none clicked
 		var dotIndex = this.dotIndexAtMouse(); //Use this to get the index of the dot clicked; returns null if none clicked
+		
+		if (dotIndex != null) {
+			if (this.lastPiecePlayed == BLACK_PLACED) {
+				state = WHITE_PLACED;
+			}
+			else {
+				state = BLACK_PLACED;
+			}
+			
+			this.board.dots[dotIndex].setState(state);
+			this.lastPiecePlayed = state;
+			this.changeDetected = true;
+		}
 		//console.log("Clicked " + this.mouseX + " " + this.mouseY);
 	}
+	
+}
+
+function updateGame(timestamp) {
+	if (game.changeDetected) {
+		console.log(game.changeDetected);
+		game.drawer.drawBoard(game.board);
+		game.changeDetected = false;
+	}
+	window.requestAnimationFrame(updateGame);
 }
 
 console.log("Hello");
@@ -194,7 +276,9 @@ const canvas = document.getElementById("c");
 const context = canvas.getContext("2d");
 const drawer = new Drawer(context);
 const board = new Board(canvas);
-var mouse = new Mouse(canvas);
+var game = new Game(board, canvas, drawer);
 drawer.drawBoard(board);
-canvas.addEventListener("mousemove", function(evt) {mouse.setMouse(evt);}, false);
-canvas.addEventListener("click", function (evt) {board.onClick(evt);}, false);
+//canvas.addEventListener("mousemove", function(evt) {mouse.setMouse(evt);}, false);
+canvas.addEventListener("click", function (evt) {game.onClick(evt);}, false);
+
+window.requestAnimationFrame(updateGame);
